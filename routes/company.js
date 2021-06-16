e8e10678dab038b895a34df72543358c1a15c0b3
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-06-16 13:52:29 +0100

    add upload photo and append passport + addCompany

--- routes/company.js
+++ routes/company.js
@@ -2,6 +2,31 @@ const express = require('express');
 const companySchema = require('../models/companySchema');
 const router = express.Router();
 const passport = require('passport');
+const multer = require('multer');
+const path = require('path');
+const bcrypt = require('bcrypt');
+
+const myStorage = multer.diskStorage({
+    destination: (req, file, callback) => {
+        const destFile = path.resolve('./uploads');
+        callback(null, destFile);
+    }, 
+    filename: (req, file, callback) => {
+        const name = Date.now() + path.extname(file.originalname);
+        callback(null, name);
+    }
+});
+
+function fileFilterFn (req, file, cb) {
+    // The function should call `cb` with a boolean
+    // to indicate if the file should be accepted
+    const extFile = path.extname(file.originalname);
+    const allowdExtension = [".jpg", "jpeg", "png"];
+    cb(null, allowdExtension.includes(extFile))
+  }
+
+//   
+const uploads = multer({ storage : myStorage, fileFilter : fileFilterFn}); 
 
 
 router.get('/company', passport.authenticate('bearer', {session : false}), async (req, res) => {
@@ -24,14 +49,44 @@ router.get('/company/:id', passport.authenticate('bearer', {session : false}), a
     res.status(200).json(company);
 })
 
-
-router.post('/company', passport.authenticate('bearer', {session : false}), async (req, res) => {
+// Create simple Company without verification
+router.post('/company', [passport.authenticate('bearer', {session : false}), uploads.single('photo') ], async (req, res) => {
+    if( req.file !== undefined){
+        req.body.photo = req.file.filename;
+    }
     const company = await companySchema.create(req.body);
     res.status(200).json(company);
 });
 
+//Create company with email check and password crypt
+router.post('/createCompany',[passport.authenticate('bearer', {session : false}), uploads.single('photo')] ,async (req, res) => {
+    const company = await companySchema.findOne({ email : req.body.email });
+    if (company) {
+        res.status(400).json({ message: "==> email existant"});
+    }
+    else{
+        try{
+            if( req.file !== undefined){
+                req.body.photo = req.file.filename;
+            }
+            const salt = await bcrypt.genSalt(10);
+            const hash = await bcrypt.hashSync(req.body.password, salt);
+            req.body.password = hash;
+            const addCompany = await companySchema.create(req.body);
+            res.json({ message: 'Created successfuly'});
+        }
+        catch (error){
+            console.log(error);
+            res.status(500).json({ message: "internal server error"});
+        }
+    }
+})
 
-router.put('/company/:id', passport.authenticate('bearer', {session : false}), async (req, res) => {
+
+router.put('/company/:id', [passport.authenticate('bearer', {session : false}), uploads.single('photo')], async (req, res) => {
+    if( req.file !== undefined){
+        req.body.photo = req.file.filename;
+    }
     const company = await companySchema.findByIdAndUpdate(req.params.id, req.body, { new: true });
     res.json(company);
 });
