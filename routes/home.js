7f5d3948decdfe964507da92287f0719ef0b96eb
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-06-21 19:14:27 +0100

    Create home API [get all events + events details]

---
+++ routes/home.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const Event = require('../models/eventSchema');
+
+
+// All Events with assosita company + tags
+router.get('/all-events', async (req, res) => {
+    const allEvents = await Event.find()
+    .populate('tags company'); 
+    res.json(allEvents);
+});
+
+// Event Details
+router.get('/event-details/:id', async (req, res) => {
+    const eventDetail = await Event.findById(req.params.id);
+    res.json(eventDetail);
+});
+
+
+module.exports = router;
