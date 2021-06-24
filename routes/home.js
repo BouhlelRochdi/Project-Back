c60c402ea887f523ff0717ecb52fa7d31d1d1d72
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-06-24 12:51:09 +0100

    Fix find =>populate

--- routes/home.js
+++ routes/home.js
@@ -6,13 +6,14 @@ const Event = require('../models/eventSchema');
 // All Events with assosita company + tags
 router.get('/all-events', async (req, res) => {
     const allEvents = await Event.find()
-    .populate('tags company'); 
+    .populate('tags').populate('company'); 
     res.json(allEvents);
 });
 
 // Event Details
 router.get('/event-details/:id', async (req, res) => {
-    const eventDetail = await Event.findById(req.params.id);
+    const eventDetail = await Event.findById(req.params.id)
+    .populate('tags').populate('company');
     res.json(eventDetail);
 });
 
